8b1c204ea584a113c6e28047a0a1e94238134afe
viniipereira <112420279+viniipereira@users.noreply.github.com>
2022-11-16 11:04:06 -0300

    Update home_page_widget.dart

--- lib/home_page/home_page_widget.dart
+++ lib/home_page/home_page_widget.dart
@@ -488,7 +488,17 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                                           0.31,
                                       height: 100,
                                       decoration: BoxDecoration(
-                                        color: Color(0xFF303030),
+                                        color: listViewDadosMotoristasRecord
+                                                    .status! ==
+                                                'Em espera'
+                                            ? Color.fromARGB(255, 214, 218, 13)
+                                            : listViewDadosMotoristasRecord
+                                                        .status! ==
+                                                    'Carregamento imediato'
+                                                ? Color.fromARGB(
+                                                    255, 23, 207, 6)
+                                                : Color.fromARGB(
+                                                    255, 211, 15, 9),
                                         borderRadius: BorderRadius.circular(0),
                                         border: Border.all(
                                           color: Color(0xFFF7F8F9),
